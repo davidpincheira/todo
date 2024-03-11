4ffd3c8e30e385b5b8b86fb1986fd4a6a9761f00
davidpincheira <david.pincheira@transgenia.com>
2024-03-11 08:42:34 -0300

    ciclo de vida de los componentes

---
+++ src/app/domains/info/about/about.component.ts
@@ -0,0 +1,25 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CounterComponent } from '../../shared/components/counter/counter.component';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [CommonModule, CounterComponent],
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {
+  duration = signal(1000);
+  message = signal('Hola');
+
+  changeDuration(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.duration.set(input.valueAsNumber) 
+  }
+
+  changeMessage(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.message.set(input.value) 
+  }
+}
