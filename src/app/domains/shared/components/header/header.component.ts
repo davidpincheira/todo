645766620a22634851ce19ba406d8285b3284985
davidpincheira <david.pincheira@transgenia.com>
2024-03-25 15:50:18 -0300

    total mediante ngOnchanges y signal

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { CommonModule } from '@angular/common';
 
@@ -12,9 +12,21 @@ import { CommonModule } from '@angular/common';
 export class HeaderComponent {
   hideSideMenu = signal(true)
   @Input({required: true}) cart: Product[] = []
+  total = signal(0);
 
   toggleSideMenu() {
     this.hideSideMenu.update(prevState =>  !prevState);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const cart = changes['cart'];
+    if (cart) {
+      this.total.set(this.calcTotal());
+    }
+  }
+
+  calcTotal() {
+    return this.cart.reduce((total, product) => total + product.price, 0);
   }
 
 }
