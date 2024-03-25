f00ede954334dece68e6e6df8fc96b1b1b558422
davidpincheira <david.pincheira@transgenia.com>
2024-03-25 17:55:07 -0300

    obteniendo datos de una api rest

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -4,6 +4,7 @@ import { ProductComponent } from '../../components/product/product.component';
 import { Product } from '../../../shared/models/product.model';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { CartService } from '../../../shared/services/cart.service';
+import { ProductService } from '../../../shared/services/product.service';
 
 @Component({
   selector: 'app-list',
@@ -15,33 +16,22 @@ import { CartService } from '../../../shared/services/cart.service';
 export class ListComponent {
 
   private cartService = inject(CartService)
+  private productService = inject(ProductService)
+
+  ngOnInit(){
+    this.productService.getProducts().subscribe({
+      next: (res)  => {
+        this.products.set(res)
+      },
+      error: (err) => {
+        console.error('Error getting products: ', err)
+      }
+    })
+  }
 
   products = signal<Product[]>([])
   //declare a variable called products that is a signal type Product and initialize empty
   //public products = signal([] as Product[])
-  constructor(){
-    const initProducts: Product[]  = [
-      {
-        id: Date.now(),
-        title: 'Pro 1',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=23',
-      },
-      {
-        id: Date.now(),
-        title: 'Pro 2',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=12',
-      },
-      {
-        id: Date.now(),
-        title: 'Pro 3',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=1212',
-      }
-    ];
-    this.products.set(initProducts);
-  }
   
   addToCart(product: Product){
     this.cartService.addToCart(product)
--- src/app/domains/shared/models/product.model.ts
+++ src/app/domains/shared/models/product.model.ts
@@ -2,5 +2,5 @@ export interface Product {
     id: number;
     title: string;
     price: number;
-    image: string;
+    images: string[];
 }
