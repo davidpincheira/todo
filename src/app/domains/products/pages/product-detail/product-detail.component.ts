b4e60ad5cafe8db6c0bc7b5def1ce892698e118d
davidpincheira <david.pincheira@transgenia.com>
2024-03-26 11:45:46 -0300

    detalle de la galeria

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,18 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, inject, signal } from '@angular/core';
 import { Product } from '@shared/models/product.model';
+import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
   @Input() id?:string;
   product = signal<Product | null>(null)
+  cover = signal('')
   private productService = inject(ProductService)
+  private cartService = inject(CartService)
 
   ngOnInit(){
     if(this.id){
@@ -20,10 +24,25 @@ export class ProductDetailComponent {
         next: (res) => {
           console.log(res)
           this.product.set(res)
+          if(res.images[0]){
+            this.cover.set(res.images[0])
+          }
         }
       })
     }
   }
+
+  changeCover(newImg: string){
+    this.cover.set(newImg)
+  }
+
+  addToCart(){
+    const product = this.product()
+    if(product){
+      this.cartService.addToCart(product)
+    }
+  }
+  
 
 
 }
--- src/app/domains/shared/models/product.model.ts
+++ src/app/domains/shared/models/product.model.ts
@@ -1,7 +1,10 @@
+import { Category } from "./category.model"
+
 export interface Product {
     id: number;
     title: string;
     price: number;
     images: string[];
     description: string;
+    category: Category
 }
