ea55c9820e51e34c7de67b874b70a8b3740de750
davidpincheira <david.pincheira@transgenia.com>
2024-03-25 23:49:01 -0300

    vistas anidadas

---
+++ src/app/domains/shared/components/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [CommonModule, HeaderComponent, RouterModule],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css'
+})
+export class LayoutComponent {
+
+}
