d3343cb3666aae57be30f9aa18c1046d3bc56282
davidpincheira <david.pincheira@transgenia.com>
2024-03-26 16:48:27 -0300

    categorias desde la api

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -5,6 +5,8 @@ import { Product } from '@shared/models/product.model';
 import { HeaderComponent } from '@shared/components/header/header.component';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
+import { Category } from '@shared/models/category.model';
 
 @Component({
   selector: 'app-list',
@@ -17,8 +19,23 @@ export class ListComponent {
 
   private cartService = inject(CartService)
   private productService = inject(ProductService)
+  private categoryService = inject(CategoryService)
 
   ngOnInit(){
+    this.getProducts()
+    this.getCategories()
+  }
+
+  products = signal<Product[]>([])
+  categories = signal<Category[]>([])
+  //declare a variable called products that is a signal type Product and initialize empty
+  //public products = signal([] as Product[])
+  
+  addToCart(product: Product){
+    this.cartService.addToCart(product)
+  }
+
+  getProducts(){
     this.productService.getProducts().subscribe({
       next: (res)  => {
         this.products.set(res)
@@ -28,12 +45,14 @@ export class ListComponent {
       }
     })
   }
-
-  products = signal<Product[]>([])
-  //declare a variable called products that is a signal type Product and initialize empty
-  //public products = signal([] as Product[])
-  
-  addToCart(product: Product){
-    this.cartService.addToCart(product)
+  getCategories(){
+    this.categoryService.getAll().subscribe({
+      next: (data)  => {
+        this.categories.set(data)
+      },
+      error: (err) => {
+        console.error('Error getting categories: ', err)
+      }
+    })
   }
 }
