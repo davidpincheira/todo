4981c08818b02c6aff2667221a438d27472a7a16
davidpincheira <david.pincheira@transgenia.com>
2024-03-28 12:39:32 -0300

    prefetching

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withComponentInputBinding()), //esta propiedad hace que los parametros le lleguen como inputs a las paginas
+    provideRouter(routes, 
+      withComponentInputBinding(), //esta propiedad hace que los parametros le lleguen como inputs a las paginas
+      withPreloading(PreloadAllModules)), //esta propiedad hace el prefetching, (carga en diferido)
     provideHttpClient()
   ]
 };
